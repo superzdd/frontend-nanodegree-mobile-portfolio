027fa87982a2bbb824f30a5b7c74d2f38ee0a47a
superzdd <superzdd@sina.com>
2017-10-23 00:46:56 +0800

    add gulp mini css and mini html for pizza

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var minifyCss = require("gulp-minify-css");
+var minifyHtml = require("gulp-minify-html");
 
 //定义一个testLess任务（自定义任务名称）
 
@@ -18,12 +20,32 @@ gulp.task('lint',
 gulp.task('scripts', function() {
 	gulp.src('./views/js/main.js')
 		.pipe(concat('all.js'))
-		.pipe(gulp.dest('./dist'))
+		.pipe(gulp.dest('./views/dist/js'))
 		.pipe(rename('all.min.js'))
 		.pipe(uglify())
-		.pipe(gulp.dest('./dist'));
-})
+		.pipe(gulp.dest('./views/dist/js'));
+});
+
+gulp.task('css', function() {
+	gulp.src('./views/css/*.css')
+		.pipe(concat('all.css'))
+		.pipe(gulp.dest('./views/dist/css'))
+		.pipe(rename('all.min.css'))
+		.pipe(minifyCss())
+		.pipe(gulp.dest('./views/dist/css'));
+});
+
+gulp.task('img', function() {
+	gulp.src('./views/images/*.*')
+		.pipe(gulp.dest('./views/dist/images'));
+});
+
+gulp.task('html', function() {
+	gulp.src('./views/pizza.html')
+		.pipe(minifyHtml())
+		.pipe(gulp.dest('./views/dist'));
+});
 
 gulp.task('default', function() {
-	gulp.run('lint', 'scripts');
+	gulp.run('img', 'scripts', 'css', 'html');
 });
